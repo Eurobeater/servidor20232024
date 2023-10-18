d2081067cfa2bee70da621f9e96bc9ddaaca5464
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-10-18 20:02:00 +0200

    Delete aulaVirtual directory

--- aulaVirtual/Database-20231006/basicos/esquema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS usuarios ;
-CREATE TABLE usuarios(
-     codigo INT NOT NULL AUTO_INCREMENT,
-     nombre VARCHAR(30) NOT NULL,
-     apellidos VARCHAR(30) NOT NULL,
-     PRIMARY KEY (codigo)
-);
--- aulaVirtual/Database-20231006/basicos/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$conexion = new PDO( "mysql:host=localhost;dbname=test", "root", "" );
-	
-	$sql = "update usuarios set nombre = 'javier' where codigo = 2";
-	
-	$conexion->exec($sql);
-
-?>
-
