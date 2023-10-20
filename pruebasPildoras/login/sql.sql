8758f6ff335cea1cdafb6cf952568144500ec084
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-10-20 10:06:07 +0200

    Delete pruebasPildoras/login directory

--- pruebasPildoras/login/sql.sql
+++
@@ -1,5 +0,0 @@
-Crear tabla
-CREATE TABLE usuarios (ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY, usuarios VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL)
-
-Meter un usuario
-INSERT INTO usuarios (ID, usuarios, password) VALUES (1, "Juan", "1234")
