19ca85e28849f3dc366561bef564ccce78d5cd1d
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-10-30 17:19:09 +0100

    Add files via upload

---
+++ funciona/cookies/destruyeCookie.php
@@ -0,0 +1,3 @@
+<?php
+setcookie("prueba", "Esta es la información de nuestra primera cookie", time() -1, "/servidor20232024/pruebasPildoras/cookies/zonaContenidos/");        //-1 destruye la cookie
+?>
