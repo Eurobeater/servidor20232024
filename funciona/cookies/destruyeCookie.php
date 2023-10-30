a77242bbdcbc648f98e437fb88268821fe8501f2
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2023-10-30 17:18:48 +0100

    Delete funciona/cookies directory

--- funciona/cookies/destruyeCookie.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-setcookie("prueba", "Esta es la información de nuestra primera cookie", time() -1, "/servidor20232024/pruebasPildoras/cookies/zonaContenidos/");        //-1 destruye la cookie
-?>
