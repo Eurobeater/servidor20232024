fdbff93cf56ef62a472b460342635599c6b13194
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2024-02-11 16:07:37 +0100

    Delete pruebasPildoras directory

--- pruebasPildoras/cookies/practica/creaCookie.php
+++
@@ -1,4 +0,0 @@
-<?php
-setcookie("idiomaSeleccionado", $_GET["idioma"], time() +1200);
-header("Location:verCookie.php");           //crea la cookie y redirige a verCookie.php, que hará que dependiendo del valor de la cookie, se muestre la página en español o en inglés
-?>
--- pruebasPildoras/cookies/practica/destruyeCookie.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("idiomaSeleccionado", "es", time() -1);       //destruir la cookie en español
-setcookie("idiomaSeleccionado", "en", time() -1);       //destruir la cookie en inglés
-header("Location:pag1.php");
-?>
