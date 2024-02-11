10d93df50394110c1c3eec6e095a7953d7184e04
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2024-02-11 16:07:08 +0100

    Delete funciona directory

--- funciona/cookies/practica/verCookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (!$_COOKIE['idiomaSeleccionado']) {       //si no existe la cookie idiomaSeleccionado
-        header("Location: pag1.php");
-    }else if ($_COOKIE["idiomaSeleccionado"] == "es") {         //si la cookie idiomaSeleccionado vale "es", redirige a spanish.php
-        header("Location:spanish.php");
-    }else if ($_COOKIE['idiomaSeleccionado'] == "en"){          //si la cookie idiomaSeleccionado vale "en", redirige a english.php
-        header("Location:english.php");
-    }
-?>
--- funciona/login/cierre.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();            //importante comenzar una sesion en el fichero de cerrar sesión, de lo contrario, no cierra
-    session_destroy();          //cerrar sesión
-    header("location:login.php");
-?>
