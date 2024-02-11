d3a02b51a0872eba716e44571cd3d44e6c9d7b8d
Eurobeater <114727608+Eurobeater@users.noreply.github.com>
2024-02-11 14:48:24 +0100

    Add files via upload

---
+++ Symfony/proyecto01/src/Controller/FactorialController.php
@@ -0,0 +1,29 @@
+<?php
+// src/Controller/FactorialController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+// ...
+
+class FactorialController extends AbstractController
+{
+    #[Route('/lucky/number')]
+    public function factorial($id): Response
+    {
+        //$number = random_int(0, 100);
+        if ($id == 1 || $id == 0) {
+            $factorial = 1;
+        }else {
+            $factorial = 1;
+            for ($i=1; $i < $id; $i++) { 
+                $factorial = $factorial * $i;
+            }
+        }
+
+        return $this->render('factorial/factorial.html.twig', [
+            'number' => "El factorial es {$factorial}" ]);
+    }
+}
+?>
